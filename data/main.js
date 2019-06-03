567c482c599aa29f8069dd23c9e9117bd64577df
Arya Ludianto <arya.pratamal@ymail.com>
2019-06-03 12:22:51 +0200

    senate HTML done and JSON statistic

---
+++ data/main.js
@@ -0,0 +1,31 @@
+
+
+
+// document.getElementById("senate-data-pre").innerHTML = JSON.stringify(data,null,2);
+
+// let members = data.results[0].members
+
+// function dispSenat () {
+// console.log("works")
+// }
+
+// function dispData (data) {  
+//   let senTab = document.getElementById("senate-data");
+
+//   data.forEach(data => {
+//     let dataVal = Object.values(data),
+//         row = document.createElement("tr");
+
+//       for (let value of dataVal) {
+//         let col = document.createElement("td"),
+//             text = document.createTextNode(value)
+//             col.appendChild(text);
+//             row.appendChild(col);
+//       }
+//     return datTab.appendChild(row) 
+//   })
+// }
+
+// $(document).ready(dispSenat);
+
+
